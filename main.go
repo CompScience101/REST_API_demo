cab5b924b51b5cb736e176f595fc84c9f1db3ad5
Nicholas <nsdonaldwebmail@gmail.com>
2022-08-07 21:21:36 -0400

    db working local, testing heroku

--- main.go
+++ main.go
@@ -1,46 +1,79 @@
 package main
 
 import (
-  "net/http"
-  "fmt"
-  "log"
-  "os"
-  "strings"
-  //"text/template"
-  "github.com/gin-gonic/gin"
-  cors "github.com/rs/cors/wrapper/gin"
-  "gorm.io/driver/postgres"
-  //"github.com/go-gorm/gorm"
-  "gorm.io/gorm"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	//"text/template"
+	"github.com/gin-gonic/gin"
+	cors "github.com/rs/cors/wrapper/gin"
+	"gorm.io/driver/postgres"
+
+	//"github.com/go-gorm/gorm"
+	"gorm.io/gorm"
 )
 
 // Book struct (Model)
 type Book struct {
-	ID     string  `json:"id" binding: "required"`
-	Isbn   string  `json:"isbn binding: "required""`
-	Title  string  `json:"title" binding: "required"`
-	Author string `json:"author_fname" binding: "required"`
+	Isbn   string `json:"isbn" binding: "required"`
+	Title  string `json:"title" binding: "required"`
+	Author string `json:"author_name" binding: "required"`
 	gorm.Model
 	//DateCreated string `json:"date_created"`
 }
 
 // Init books var as a slice Book struct
 var books []Book
+var db *gorm.DB
 
-func health (c *gin.Context) { // monitor health of api
-    c.JSON(http.StatusOK, nil)  
+func health(c *gin.Context) { // monitor health of api
+	c.JSON(http.StatusOK, nil)
 	fmt.Printf("replied to client health check with success status.")
 }
-func test (c *gin.Context){
+func test(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"test": "testing connection", "test2": "github actions ci"})
 }
+
+func createbook(c *gin.Context) {
+	var myjson Book
+	if err := c.BindJSON(&myjson); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": string(err.Error())})
+		fmt.Printf("Createbook invalid request")
+		return
+	}
+	fmt.Printf("sending book to db")
+	result := db.Create(&myjson)
+	if result.Error == nil {
+		c.JSON(http.StatusCreated, myjson)
+		fmt.Printf("created book. sending client response \n")
+		return
+	} else {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error})
+		fmt.Printf("error creating book in db. sending client response \n")
+	}
+
+}
+func getbooks(c *gin.Context) {
+	result := db.Find(&books)
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": string(result.Error.Error())})
+		fmt.Printf("db error while retrieving all books")
+		return
+	}
+	if result == nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "no records available"})
+		fmt.Printf("db error, no books while retrieving all books")
+		return
+	}
+	c.JSON(http.StatusOK, books)
+	fmt.Printf("retrieved all books. sent client response \n")
+	return
+}
+
 /*
-func createbook (){
-
-}
-func getbooks (){
-
-}
 func getbook (){
 
 }
@@ -52,7 +85,7 @@ func deletebook {}
 func updatebookpatch {}
 
 func transaction (c *gin.Context){ // process customer transaction
-	var myjson MyTransaction 
+	var myjson MyTransaction
 	if c.BindJSON(&myjson) != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid payment, please make sure you use a integer with no decimal places."})
 		fmt.Printf("invalid payment, user needs to instead use a integer with no decimal places.")
@@ -63,7 +96,7 @@ func transaction (c *gin.Context){ // process customer transaction
 	c.JSON(http.StatusCreated, gin.H{"reward": points})
 	fmt.Printf("sent reward client %d  points \n", points )
 	return
-	//c.Data(200, "application/json; charset=utf-8", []byte(string(dat)))	
+	//c.Data(200, "application/json; charset=utf-8", []byte(string(dat)))
 }
 
 func calculateTransaction (cost int64)(int64) { // calculate customer reward points
@@ -88,123 +121,131 @@ func calculateTransaction (cost int64)(int64) { // calculate customer reward poi
 
 */
 
-
-
-
-
 func main() { // setup router and api
-	
-  r := gin.Default()
-  r.Use(cors.Default())
-  r.HEAD("/api/health/", health)
-  r.GET("/", test)
-  /*
-  r.POST("/api/createbook", createbook)
-  r.GET("/api/getbooks/", getbooks)
-  r.GET("/api/getbook/{id}", getbook)
-  r.PUT("/api/updatebook/{id}", updatebook)
-  r.DELETE("/api/deletebook/{id}", deletebook)
-  */
-  //r.PATCH("/api/updatebook/patch/{id}", updatebookpatch)
-  
-   /* test
-   s := "This,is,a,delimited,string"
-   v := strings.Split(s, ",")  
-    for i := range v {
-        p := v[i]
-        fmt.Println(p)
-    }
-   fmt.Println("v %v", v)     // [This is a delimited string]  
-  */
-  /* test
-  s1 := "postgres://jsfdkljkdsfkljsljkfds:alkjdfsjlkfsdlkfjjdslkafjlksdsdfjljsdflkljdsjfkldsjklflkf@dsklfkdsf.com:111111/jihuguyrtdfgyuhujkopijuy" ; User,Password,host,port,dbname
-  temp1 := strings.Split(s1, "://")//remove
-  v1 := temp1[1]
-  fmt.Println("v1 ",v1)
-  temp2 := strings.Split(v1, ":")
-  v2 := temp2[0]
-  v3 := temp2[1]
-  v4 := temp2[2]
-  fmt.Println("user ",v2)
-  fmt.Println("v3 ",v3)
-  temp3 := strings.Split(v3, "@")
-  v5 := temp3[0]
-  v6 := temp3[1]
-  fmt.Println("password ",v5)
-  fmt.Println("host ",v6)
-  temp4 := strings.Split(v4, "/")
-  v7 := temp4[0]
-  v8 := temp4[1]
-  fmt.Println("port ",v7)
-  fmt.Println("dbname ",v8)
-  // v := strings.Split(s, "://")  
-   // for i := range v {
-   //     p := v[i]
-	//	fmt.Println("split ", p)
-    //    fmt.Printf("%v", s)
-    //}
-  */
-   
-  /* manage db */
-  fmt.Println("preparing db")
-  // refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-  // CLI TOOL postgres://jsfdkljkdsfkljsljkfds:alkjdfsjlkfsdlkfjjdslkafjlksdsdfjljsdflkljdsjfkldsjklflkf@dsklfkdsf.com:111111/asjkdlfjklfdsklfjsdlakjfsdjflksdjkf
-  // // format for gorm connection // dsn := "dbname=asjkdlfjklfdsklfjsdlakjfsdjflksdjkf host=dsklfkdsf.com port=111111 user=jsfdkljkdsfkljsljkfds password=alkjdfsjlkfsdlkfjjdslkafjlksdsdfjljsdflkljdsjfkldsjklflkf sslmode=require TimeZone=America/New_York""
-  // INFORMATION FROM https://data.heroku.com/ "DATABASE CREDENTIALS"; go https://data.heroku.com/ then to app then "settings" then credentials
-   
-  
-  //use db url
-  s1 := os.Getenv("DATABASE_URL") // DATABASE_URL
-  temp1 := strings.Split(s1, "://")//remove
-  v1 := temp1[1]
-  fmt.Println("v1 ",v1)
-  temp2 := strings.Split(v1, ":")
-  v2 := temp2[0]
-  v3 := temp2[1]
-  v4 := temp2[2]
-  fmt.Println("user ",v2)
-  fmt.Println("v3 ",v3)
-  temp3 := strings.Split(v3, "@")
-  v5 := temp3[0]
-  v6 := temp3[1]
-  fmt.Println("password ",v5)
-  fmt.Println("host ",v6)
-  temp4 := strings.Split(v4, "/")
-  v7 := temp4[0]
-  v8 := temp4[1]
-  fmt.Println("port ",v7)
-  fmt.Println("dbname ",v8)
 
-  dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=require TimeZone=America/New_York", v6, v2, v5, v8, v7)
-  db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-  fmt.Println("dsn ", dsn )
-  fmt.Println("DB_HOST ",os.Getenv("DB_HOST")," DB_USER ",os.Getenv("DB_USER")," DB_PASSWORD ",os.Getenv("DB_PASSWORD")," DB_NAME ",os.Getenv("DB_NAME"), " DB_PORT ", os.Getenv("DB_PORT") )
-  if(err != nil){ 
-	fmt.Println("db connection error") 
-	log.Fatal("could not connect to db");
-  } 
-  fmt.Println("db connection made")  
-  bool := db.Migrator().HasTable(&Book{})//check status
-  if(bool == true){
-	
-	fmt.Println("already created db table(s)")
-  }else{
-    db.Migrator().CreateTable(&Book{})
-	bool := db.Migrator().HasTable(&Book{})
-	if(bool == true){
-		fmt.Println("created db table(s)")
-	}else{
-		fmt.Println("failed to create db table(s)")
+	r := gin.Default()
+	r.Use(cors.Default())
+	r.HEAD("/api/health/", health)
+	r.GET("/", test)
+	r.POST("/api/createbook", createbook)
+	r.GET("/api/getbooks/", getbooks)
+	/*
+		    r.GET("/api/getbook/{id}", getbook)
+			  r.PUT("/api/updatebook/{id}", updatebook)
+			  r.DELETE("/api/deletebook/{id}", deletebook)
+	*/
+	//r.PATCH("/api/updatebook/patch/{id}", updatebookpatch)
+
+	/* test
+	   s := "This,is,a,delimited,string"
+	   v := strings.Split(s, ",")
+	    for i := range v {
+	        p := v[i]
+	        fmt.Println(p)
+	    }
+	   fmt.Println("v %v", v)     // [This is a delimited string]
+	*/
+	/* test
+	  s1 := "postgres://jsfdkljkdsfkljsljkfds:alkjdfsjlkfsdlkfjjdslkafjlksdsdfjljsdflkljdsjfkldsjklflkf@dsklfkdsf.com:111111/jihuguyrtdfgyuhujkopijuy" ; User,Password,host,port,dbname
+	  temp1 := strings.Split(s1, "://")//remove
+	  v1 := temp1[1]
+	  fmt.Println("v1 ",v1)
+	  temp2 := strings.Split(v1, ":")
+	  v2 := temp2[0]
+	  v3 := temp2[1]
+	  v4 := temp2[2]
+	  fmt.Println("user ",v2)
+	  fmt.Println("v3 ",v3)
+	  temp3 := strings.Split(v3, "@")
+	  v5 := temp3[0]
+	  v6 := temp3[1]
+	  fmt.Println("password ",v5)
+	  fmt.Println("host ",v6)
+	  temp4 := strings.Split(v4, "/")
+	  v7 := temp4[0]
+	  v8 := temp4[1]
+	  fmt.Println("port ",v7)
+	  fmt.Println("dbname ",v8)
+	  // v := strings.Split(s, "://")
+	   // for i := range v {
+	   //     p := v[i]
+		//	fmt.Println("split ", p)
+	    //    fmt.Printf("%v", s)
+	    //}
+	*/
+
+	/* manage db */
+	fmt.Println("preparing db")
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	// CLI TOOL postgres://jsfdkljkdsfkljsljkfds:alkjdfsjlkfsdlkfjjdslkafjlksdsdfjljsdflkljdsjfkldsjklflkf@dsklfkdsf.com:111111/asjkdlfjklfdsklfjsdlakjfsdjflksdjkf
+	// // format for gorm connection // dsn := "dbname=asjkdlfjklfdsklfjsdlakjfsdjflksdjkf host=dsklfkdsf.com port=111111 user=jsfdkljkdsfkljsljkfds password=alkjdfsjlkfsdlkfjjdslkafjlksdsdfjljsdflkljdsjfkldsjklflkf sslmode=require TimeZone=America/New_York""
+	// INFORMATION FROM https://data.heroku.com/ "DATABASE CREDENTIALS"; go https://data.heroku.com/ then to app then "settings" then credentials
+
+	//use db url
+	s1 := os.Getenv("DATABASE_URL")   // DATABASE_URL
+	temp1 := strings.Split(s1, "://") //remove
+	v1 := temp1[1]
+	fmt.Println("v1 ", v1)
+	temp2 := strings.Split(v1, ":")
+	v2 := temp2[0]
+	v3 := temp2[1]
+	v4 := temp2[2]
+	fmt.Println("user ", v2)
+	fmt.Println("v3 ", v3)
+	temp3 := strings.Split(v3, "@")
+	v5 := temp3[0]
+	v6 := temp3[1]
+	fmt.Println("password ", v5)
+	fmt.Println("host ", v6)
+	temp4 := strings.Split(v4, "/")
+	v7 := temp4[0]
+	v8 := temp4[1]
+	fmt.Println("port ", v7)
+	fmt.Println("dbname ", v8)
+
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=require TimeZone=America/New_York", v6, v2, v5, v8, v7)
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	//fmt.Printf("gorm db type %T",db)
+	fmt.Println("dsn ", dsn)
+	fmt.Println("DB_HOST ", os.Getenv("DB_HOST"), " DB_USER ", os.Getenv("DB_USER"), " DB_PASSWORD ", os.Getenv("DB_PASSWORD"), " DB_NAME ", os.Getenv("DB_NAME"), " DB_PORT ", os.Getenv("DB_PORT"))
+	if err != nil {
+		fmt.Println("db connection error")
+		log.Fatal("could not connect to db")
 	}
-  }
-  fmt.Println("preparing router") 
-  port := os.Getenv("PORT")
-  if port == "" { //heroku assignment fail
+	fmt.Println("db connection made")
+
+	bool := db.Migrator().HasTable(&Book{}) //check status
+	if bool == true {
+
+		fmt.Println("already created db table(s)")
+	} else {
+		db.Migrator().CreateTable(&Book{})
+		bool := db.Migrator().HasTable(&Book{})
+		if bool == true {
+			fmt.Println("created db table(s)")
+		} else {
+			fmt.Println("failed to create db table(s)")
+		}
+	}
+
+	/* delete old table
+	db.Migrator().DropTable(&Book{})
+	bool := db.Migrator().HasTable(&Book{})
+	if bool == true {
+		fmt.Println("db table(s) still exist")
+	} else {
+		fmt.Println("deleted db table(s)")
+	}
+	*/
+	fmt.Println("preparing router")
+	port := os.Getenv("PORT")
+	if port == "" { //heroku assignment fail
 		r.Run(":8080")
-		fmt.Println("started router") 
+		fmt.Println("started router")
 		//log.Fatal("$PORT must be set")//let heroku log error
-  }
-  r.Run(":" + port)
-  
-  fmt.Println("started router") 
+	}
+	r.Run(":" + port)
+
+	fmt.Println("started router")
 }
